da4931a0f9565e98e4cee224dc7549a3666d861f
chloe24603 <chloe24603@gmail.com>
2022-01-28 15:00:02 +0000

    Object Movement Script
    Implemented a basic script to move objects when clicked. This script will be improved upon at a later date and optimized if there is time.

---
+++ SpongetasticGame/Assets/Scripts/MoveObjects.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjects : MonoBehaviour
+{
+    Vector3 startPosition;
+    GameObject clickedObject;
+    [SerializeField] float objectX;
+    [SerializeField] float objectY;
+    Vector3 movePosition;
+
+    private void Start()
+    {
+        startPosition = this.gameObject.transform.position;
+        movePosition = new Vector3(objectX, objectY, this.gameObject.transform.position.z);
+    }
+    private void OnMouseDown()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit.collider != null)
+        {
+            if (hit.transform.gameObject.CompareTag("Moveable"))
+            {
+                clickedObject = hit.transform.gameObject;
+                if (clickedObject == this.gameObject)
+                {
+                    if (this.transform.position == startPosition)
+                    {
+                        this.gameObject.transform.position = movePosition;
+                    }
+                    else if (this.transform.position == movePosition)
+                    {
+                        this.gameObject.transform.position = startPosition;
+                    }
+                }
+            }
+        }
+    }
+
+}
