298c444a85d664f72d4ff472f2f4fa88dc5b4cc0
slitthegoblin <73849505+slitthegoblin@users.noreply.github.com>
2022-02-15 13:51:28 +0000

    audio and music GUI
    auydio and music GUI

--- SpongetasticGame/Assets/Scripts/Yasemin/PauseMenu.cs
+++ SpongetasticGame/Assets/Scripts/Yasemin/PauseMenu.cs
@@ -46,7 +46,7 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu()
     {
         Debug.Log("Loading Menu");
-        SceneManager.LoadScene("MainMenuScene");
+        SceneManager.LoadScene("MainMenuSceneud");
     }
 
     public void QuitGame()
