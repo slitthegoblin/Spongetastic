23c027269714a1c9e002d704d5cd3143478f3acf
chloe24603 <chloe24603@gmail.com>
2022-03-11 15:00:48 +0000

    Updating Top Down Map and Enter and Exit Level Script

---
+++ SpongetasticGame/Assets/Scripts/Chloe and Louis/EnterAndExit.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EnterAndExit : MonoBehaviour
+{
+
+    public int iLevelToLoad;
+    public string sLevelToLoad;
+
+    public bool useIntegerToLoadLevel = false;
+
+    [SerializeField] int levelNumber;
+    int currentLevel = LevelScript.LS.playerLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+
+        if(collisionGameObject.name == "Player")
+        {
+            LoadScene();
+        }
+    }
+
+    public void LoadScene()
+    {
+        currentLevel = LevelScript.LS.playerLevel;
+
+        if (useIntegerToLoadLevel)
+        {
+            SceneManager.LoadScene(iLevelToLoad);
+        }
+         else
+        {
+            if (currentLevel == 0 || currentLevel == levelNumber) {
+                SceneManager.LoadScene(sLevelToLoad);
+            }
+        }
+    }
+}
