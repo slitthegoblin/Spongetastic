5229bf2eedc0302515af9c3ff74f28004a713228
slitthegoblin <73849505+slitthegoblin@users.noreply.github.com>
2022-03-20 17:58:32 +0000

    we
    ew

--- SpongetasticGame/Assets/Scripts/Yasemin/SoundManager.cs
+++ SpongetasticGame/Assets/Scripts/Yasemin/SoundManager.cs
@@ -12,56 +12,57 @@ public class SoundManager : MonoBehaviour
 
     private void Start()
     {
-        if (!PlayerPrefs.HasKey("muted"))
-        {
-            PlayerPrefs.SetInt("muted", 0);
-            Load();
-        }
-        else
-        {
-            Load();
-        }
-        UpdateButtonIcon();
-        AudioListener.pause = muted;
-    }
+        //    if (!PlayerPrefs.HasKey("muted"))
+        //    {
+        //        PlayerPrefs.SetInt("muted", 0);
+        //        Load();
+        //    }
+        //    else
+        //    {
+        //        Load();
+        //    }
+        //    UpdateButtonIcon();
+        //    AudioListener.pause = muted;
+        //}
 
-    public void OnButtonPress()
-    {
-        if(muted == false)
-        {
-            muted = true;
-            AudioListener.pause = true;
-        }
-        else
-        {
-            muted = false;
-            AudioListener.pause = false;
-        }
+        //public void OnButtonPress()S
+        //{
+        //    if(muted == false)
+        //    {
+        //        muted = true;
+        //        AudioListener.pause = true;
+        //    }
+        //    else
+        //    {
+        //        muted = false;
+        //        AudioListener.pause = false;
+        //    }
 
-        Save();
-        UpdateButtonIcon();
-    }
+        //    Save();
+        //    UpdateButtonIcon();
+        //}
 
-    private void UpdateButtonIcon()
-    {
-        if(muted == false)
-        {
-            soundOnIcon.enabled = true;
-            soundOffIcon.enabled = false;
-        }
-        else
-        {
-            soundOnIcon.enabled = false;
-            soundOffIcon.enabled = true;
-        }
-    }
+        //private void UpdateButtonIcon()
+        //{
+        //    if(muted == false)
+        //    {
+        //        soundOnIcon.enabled = true;
+        //        soundOffIcon.enabled = false;
+        //    }
+        //    else
+        //    {
+        //        soundOnIcon.enabled = false;
+        //        soundOffIcon.enabled = true;
+        //    }
+        //}
 
-    private void Load()
-    {
-        muted = PlayerPrefs.GetInt("muted") == 1;
-    }
-    private void Save()
-    {
-        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
+        //private void Load()
+        //{
+        //    muted = PlayerPrefs.GetInt("muted") == 1;
+        //}
+        //private void Save()
+        //{
+        //    PlayerPrefs.SetInt("muted", muted ? 1 : 0);
+        //}
     }
 }
